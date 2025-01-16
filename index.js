1a348a24ba40232e75749ec5d8c72efada6ec924
AnOrEk <96070284+AnOrEk123@users.noreply.github.com>
2025-01-16 09:40:51 +0300

    change side-bar opening and add fixed header

--- index.js
+++ index.js
@@ -3,42 +3,48 @@ const menuBtn = document.querySelector(".menu");
 canvas = document.getElementById("can");
 const ctx = canvas.getContext("2d");
 let changed = false;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let gridSize = 40;
+
+
 
 menuBtn.addEventListener("click", () => {
-  menuBtn.classList.contains("active")
-    ? (menuBtn.classList.remove("active"),
-      root.style.setProperty("--side-size", "0rem"))
-    : (menuBtn.classList.add("active"),
-      root.style.setProperty("--side-size", "20rem"));
+  if (menuBtn.classList.contains("active")) {
+      menuBtn.classList.remove("active");
+      root.style.setProperty("--side-size", "0rem");
+  } else {
+      menuBtn.classList.add("active");
+      root.style.setProperty("--side-size", "20rem");
+  }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight - 64;
+  draw();
+});
 
-  function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < canvas.height; i += 40) {
-      ctx.beginPath();
-      ctx.moveTo(0, i);
-      ctx.lineTo(canvas.width, i);
-      ctx.stroke();
-    }
-
-    for (let i = 0; i < canvas.width; i += 40) {
-      ctx.beginPath();
-      ctx.moveTo(i, 0);
-      ctx.lineTo(i, canvas.height);
-      ctx.stroke();
-    }
+  for (let i = 0; i < canvas.height; i += gridSize) {
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(canvas.width, i);
+    ctx.stroke();
   }
 
-  window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - 64;
-    draw();
-  });
+  for (let i = 0; i < canvas.width; i += gridSize) {
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, canvas.height);
+    ctx.stroke();
+  }
+}
 
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  root.style.setProperty("--side-size", "0rem");
+  menuBtn.classList.remove("active");
   draw();
 });
